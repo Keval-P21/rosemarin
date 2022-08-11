a17bd57b1bf65f041ef4fc0e562e88f010bbd270
Freya Caudwell <freyacaudwell@gail.com>
2022-08-11 11:33:22 +0100

    backend testing fixed

--- server/Test/userController.test.js
+++ server/Test/userController.test.js
@@ -1,57 +1,23 @@
-//Abandonned testing for this sprint
+const request = require('supertest')('http://localhost:3001');
 
-// const router = require('../router');
-// const supertest = require('supertest');
-// const app = require('../index');
+const account = {
+  email: 'test@gmail.com',
+  password: 'password',
+};
 
-// const User = require('../models/User');
-// const Sequelize = require('sequelize');
-// // const app = express();
-// const request = require('supertest')(app);
+let loginResponse;
 
-// // session cookie Session {
-// //   cookie: {
-// //     path: '/',
-// //     _expires: 2022-08-10T16:16:59.849Z,
-// //     originalMaxAge: 3600000,
-// //     httpOnly: true
-// //   },
-// //   sid: 1
-// // }
-// //Hashed
-// // cookies: {
-// //   sid: 's:adrTZFsP9xJRlT1mGMjvaZAlPTxvpWlP.bm039LSQzFDaFMxbI6wKDaoiXfwLoIYfXPJ/x05jpE0'
-// // },
-
-// const account = {
-//   email: 'test@gmail.com',
-//   password: 'password',
-// };
-
-// const login = function (request) {
-//   request
-//     .post('/login')
-//     .send(account)
-//     .end(function (err, res) {
-//       if (err) {
-//         throw err;
-//       }
-//       agent.saveCookies(res);
-//     });
-// };
-
-// describe('User API', () => {
-//   beforeEach(function () {
-//     login(
-//       request
-//       //   , function (loginAgent) {
-//       //   agent = loginAgent;
-//       // }
-//     );
-//   });
-//   it('should allow access to /me route when logged in', async () => {
-//     const req = request.get('/me');
-//     agent.attachCookies(req);
-//     req.expect(200);
-//   });
-// });
+describe('User API - Authenticated path', () => {
+  beforeEach(async () => {
+    loginResponse = await request.post('/login').send(account);
+  });
+  it('should allow access to /me route when logged in', async () => {
+    const response = await request
+      .get('/me')
+      .set('Cookie', [loginResponse.header['set-cookie'][0]]);
+    console.log(response.body);
+    expect(response.status).toBe(200);
+    expect(response.body.sid).toBe(1);
+    expect(response.body.isAuthenticated).toBe(true);
+  });
+});
